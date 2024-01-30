f27710ad11d4f023149dd4218ef06ee129c43c66
Marco Herrera-Rendon <mherrerarendon@makemusic.com>
2024-01-29 19:58:27 -0700

    removed logs

--- src/create_response.rs
+++ src/create_response.rs
@@ -10,31 +10,18 @@ fn questions_as_answers(questions: &[DnsQuestion]) -> Vec<DnsAnswer> {
     questions
         .iter()
         .map(|question| {
-            print!("question into");
             let mut answer: DnsAnswer = question.into();
-            print!("resolve answer");
             answer.resolve();
             answer
         })
         .collect()
 }
 pub fn create_response(query_bytes: &[u8; 512]) -> Vec<u8> {
-    println!("Deserializing query");
     let (header, questions, _) = DnsPacket::deserialize(query_bytes).1.into_parts();
-    println!("Got {} questions", questions.len());
-    println!("question 1: {:?}", &questions[0]);
-    println!("got header {:?}", header);
-
-    println!("Creating response header");
     let response_header = DnsHeader::create_response(header);
-
-    println!("Creating answers");
     let answers = questions_as_answers(&questions);
-
-    println!("Creating packet");
     let dns_packet = DnsPacket::new(response_header, questions, answers);
 
-    print!("Serializing packet");
     dns_packet.serialize()
 }
 
