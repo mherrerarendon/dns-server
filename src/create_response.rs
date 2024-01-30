e43230d96dea04668df63ed47c627c5d0fd444df
Marco Herrera-Rendon <mherrerarendon@makemusic.com>
2024-01-29 19:17:18 -0700

    remove log

--- src/create_response.rs
+++ src/create_response.rs
@@ -9,6 +9,7 @@ pub fn create_response(query_bytes: &[u8; 512]) -> Vec<u8> {
     println!("Deserializing query");
     let (header, questions, _) = DnsPacket::deserialize(query_bytes).1.into_parts();
     println!("Got {} questions", questions.len());
+    println!("question 1: {:?}", questions[0].name);
 
     println!("Creating response header");
     let response_header = DnsHeader::create_response(header);
